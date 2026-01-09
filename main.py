53d02eacc5c9689a08786278b0da3c9e264e94d6
s332630 <s332630@mail.math.uni.wroc.pl>
2026-01-09 11:43:06 +0100

    add reqs and main update

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
-# This is a sample Python script.
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def get_random_quote():
+    try:
+        response = requests.get("https//api.quotable.io")
+        if response.status_code == 200:
+            data = response.json()
+            return data["content"]
+        else:
+            return f"{response.status_code}"
+    except Exception as error:
+        return f"{error}"
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+if __name__ == "__main__":
+    print(get_random_quote())
